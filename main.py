a32595d4a5459fe3f0e597f79a5e71780298728e
canbulatb <126672142+canbulatb@users.noreply.github.com>
2023-11-29 18:52:16 +0100

    .
    .

---
+++ main.py
@@ -0,0 +1,5 @@
+#pyuic5 -x crm_giris.ui -o crm_giris.py
+#pyuic5 -x crm_ara_ekran.ui -o crm_ara_ekran.py
+#pyuic5 -x crm_basvuru.ui -o crm_basvuru.py
+#pyuic5 -x crm_mentor.ui -o crm_mentor.py
+#pyuic5 -x crm_mulakat.ui -o crm_mulakat.py
